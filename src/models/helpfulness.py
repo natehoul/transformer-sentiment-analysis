15aa3915746eaf17ec8de58f8504c3ac62cadc6b
James Simmons <jsimmons@jmms.me>
2022-04-14 18:38:53 -0400

    Added a models and training package to src

---
+++ src/models/helpfulness.py
@@ -0,0 +1,3 @@
+# TODO: A pytorch model to predict review helpfulness
+# Could be classification or regression
+# Lower priority than ratings.py
---
+++ src/models/ratings.py
@@ -0,0 +1,6 @@
+from datasets.amazon_review.amazonReviewsDatasets import AmazonReviewsDataset
+
+# TODO: pytorch model to predict rating from the text of a review
+# Use the AmazonReviewsDataset, which is a three-class system of 0 = bad, 1 = neutral, 2 = good
+# Consider changing to a binary classifier by compining bad and neutral
+# High priority
