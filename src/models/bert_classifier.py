dff27d067ea89f89611eb6985005d92b8a47beef
James Simmons <jsimmons@jmms.me>
2022-04-15 18:21:56 -0400

    created the file for the BertClassifer

---
+++ src/models/bert_classifier.py
@@ -0,0 +1,5 @@
+# TODO: Recreate the BERT classifier from HW4
+# Basically just a combination of BertModel.from_pretrained for embedding and TransformerEncoder for classification
+# Likely to be similar to BertClassifier from HW4
+# This is the model we'll train first
+
