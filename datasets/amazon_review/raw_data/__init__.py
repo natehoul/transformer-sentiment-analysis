84223f90c2b94e32852e60b968807c1f0f6d9b82
James Simmons <jsimmons@jmms.me>
2022-04-15 18:08:28 -0400

    extracted resolve_alias, added musical instruments

--- datasets/amazon_review/raw_data/__init__.py
+++ datasets/amazon_review/raw_data/__init__.py
@@ -4,19 +4,36 @@ import os
 # Keys should all be lower case
 raw_data = \
     {
-        'tools and home improvement': ('Tools_and_Home_Improvement_5', 2_070_831)
+        'tools_and_home_improvement': ('Tools_and_Home_Improvement_5', 2_070_831),
+        'musical_instruments': ('Musical_Instruments_5', 231_392)
     }
 
 # A dictionary containing all of the different ways you can refer to a given dataset
 # Keys should match a key in raw_data
 aliases = \
     {
-        'tools and home improvement': {'thi', 'tahi', 'tools'}
+        'tools_and_home_improvement': {'thi', 'tahi', 'tools'},
+        'musical_instruments': {'music', 'mi', 'instruments'}
     }
 
 
 # The possible extensions to try, in typical priority order
 extensions = ['.pkl', '.json.gz']
+
+
+# Converts an alias to its true key name
+# Returns None if key not found
+def resolve_alias(alias: str) -> str or None:
+    alias = alias.lower()
+    if alias in raw_data:
+        return alias
+
+    else:
+        for key, alias_set in aliases.items():
+            if alias in alias_set:
+                return key
+    
+    return None
 
 
 # Input:
@@ -34,21 +51,16 @@ extensions = ['.pkl', '.json.gz']
 #   This also enforces all lower case, which makes things easier
 #   Also dynamically selects the pickle data over the json.gz data, unless otherwise specified
 def get_raw_data(alias: str, prefer_pickle: bool=True):
-    alias = alias.lower()
-    if alias not in raw_data:
-        for key, alias_set in aliases.items():
-            if alias in alias_set:
-                alias = key
-                break
+    key = resolve_alias(alias)
 
-    filename, size = raw_data[alias]
+    filename, size = raw_data[key]
     filename = os.path.dirname(__file__) + '/' + filename
     extension_order = extensions[::1] if prefer_pickle else extensions[::-1]
     extension_order.append(None)
 
     for extension in extension_order:
         if extension is None:
-            raise FileNotFoundError(f'Neither .json.gz nor .pkl exists for {alias}')
+            raise FileNotFoundError(f'Neither .json.gz nor .pkl exists for {key}')
 
         path = filename + extension
         if os.path.isfile(path):
