33e1133e549b24e158882d035f857cc6b7075f8f
James Simmons <jsimmons@jmms.me>
2022-04-15 18:06:51 -0400

    drop N/A values, dataset now optionally 2-class

--- datasets/amazon_review/amazonReviewsDatasets.py
+++ datasets/amazon_review/amazonReviewsDatasets.py
@@ -13,7 +13,7 @@ selected_columns = ['reviewText', 'overall']
 	# This maybe shouldn't be in the datasets/amazon-review directory, but instead teh datasets/review_helpfulness dir (?)
 # TODO: Use logging instead of printing to stdout; ideally global logging for the whole program (init in some __init__.py file somewhere)
 class AmazonReviewsDataset(Dataset):
-	def __init__(self, dataset_name: str, save_pickle: bool=True):
+	def __init__(self, dataset_name: str, save_pickle: bool=True, binarize=True):
 		is_pickle, path, size = get_raw_data(dataset_name)
 
 		if is_pickle:
@@ -21,7 +21,7 @@ class AmazonReviewsDataset(Dataset):
 			df = pd.read_pickle(path)
 		else:
 			df = loadData.getDF(path, size)
-			df = df[selected_columns]
+			df = df[selected_columns].dropna()
 			
 			if save_pickle:
 				pickle_name = path.replace('.json.gz', '.pkl')
@@ -31,8 +31,8 @@ class AmazonReviewsDataset(Dataset):
 		self.review_text = df['reviewText'].copy()
 		self.labels = df['overall'].copy()
 		self.labels[self.labels < 3] = 0
-		self.labels[self.labels == 3] = 1
-		self.labels[self.labels > 3] = 2
+		self.labels[self.labels == 3] = 0 if binarize else 1
+		self.labels[self.labels > 3] = 1 if binarize else 2
 
 	def __len__(self):
 		return len(self.labels)
@@ -44,3 +44,13 @@ class AmazonReviewsDataset(Dataset):
 		# Also if there's a problem with the embeddings, we won't know until we start training
 		# Correction: It just occurred to me that the embeddings change slightly as the embedding model trains, so redoing the work is necessary
 		return bertEmbed.get_token_embeddings(self.review_text.iloc[idx]), self.labels.iloc[idx]
+
+
+	def get_all_data(self):
+		return self.review_text.to_numpy(), self.labels.to_numpy()
+
+
+
+# For testing stuff
+if __name__ == "__main__":
+	dataset = AmazonReviewsDataset('tools')
