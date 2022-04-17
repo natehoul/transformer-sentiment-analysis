Fixed embedding issues, current issue is with something to do with gradient function

src/models/bert_classifier.py
@@ -6,20 +6,22 @@
 from pprint import pprint
 import sys
 
+import torch
 import torch.nn as nn
 from transformers import BertModel
 from transformer import TransformerEncoder
 
+import numpy as np
 
 
 # Create the BertClassfier class
 class BertClassifier(nn.Module):
     """Bert Model for Classification Tasks.
     """
-    def __init__(self, num_classes: int = 2, num_layers: int = 6, dim_model: int = 512, num_heads: int = 8, dim_feedforward: int = 2048, dropout: float = 0.1):
+    def __init__(self, num_classes: int = 2, num_layers: int = 6, dim_model: int = 768, num_heads: int = 8, dim_feedforward: int = 2048, dropout: float = 0.1):
         super(BertClassifier, self).__init__()
         # Instantiate BERT model
-        self.bert = BertModel.from_pretrained('bert-base-uncased')
+        self.bert = BertModel.from_pretrained('bert-base-uncased', output_hidden_states=True)
 
         # Instantiate a transformer-based classifer
         self.classifier = TransformerEncoder(output_size=num_classes, 
@@ -30,7 +32,7 @@ class BertClassifier(nn.Module):
                                             dropout=dropout)
 
         
-    def forward(self, input_ids, attention_mask):
+    def forward(self, tokens_tensor, attention_mask):
         """
         Feed input to BERT and the classifier to compute logits.
         @param    input_ids (torch.Tensor): an input tensor with shape (batch_size,
@@ -41,15 +43,39 @@ class BertClassifier(nn.Module):
                       num_labels)
         """
         # Feed input to BERT
-        outputs = self.bert(input_ids=input_ids,
-                            attention_mask=attention_mask)
+
+        segments_ids = [1] * tokens_tensor.size(1)
+
+        segments_tensor = torch.tensor([segments_ids]).to(tokens_tensor.get_device())
+	
+
+        with torch.no_grad():
+                outputs = self.bert(tokens_tensor, segments_tensor) # NOTE: Error here. See belohidden_states = outputs[2]
+                hidden_states = outputs[2]
+
+        token_embeddings = torch.stack(hidden_states, dim=0)
+        token_embeddings = torch.squeeze(token_embeddings, dim=1)
+        token_embeddings = token_embeddings.permute(1, 0, 2, 3)
+        token_vecs_sum = []
+        for token in token_embeddings:
+                sum_vec = torch.sum(token[-4:], dim=0)        
+                token_vecs_sum.append(sum_vec)
+        #print(token_vecs_sum.shape)
+        embeddings = torch.stack(token_vecs_sum).to(tokens_tensor.get_device())
+        print('EMBEDDING SIZE')
+        print(embeddings.size())
+        #embedding = torch.tensor([token_vecs_sum]).to(tokens_tensor.get_device())
+        #print(embedding.size())
+
+        #outputs = self.bert(input_ids=input_ids,
+        #                    attention_mask=attention_mask)
         
         # Extract the last hidden state of the token `[CLS]` for classification task
         # TBH I'm not entirely sure what this does or why it works
-        last_hidden_state_cls = outputs[0][:, 0, :]
+        #last_hidden_state_cls = outputs[0][:, 0, :]
 
         # Feed input to classifier to compute logits
-        logits = self.classifier(last_hidden_state_cls)
+        logits = self.classifier(embeddings)
 
         return logits
 

src/models/transformer.py
@@ -3,7 +3,7 @@ from torch import Tensor
 from torch import nn
 import torch.nn.functional as f
 
-def scaled_dot_product_attention(query, Tensor, key: Tensor, value: Tensor) -> Tensor:
+def scaled_dot_product_attention(query: Tensor, key: Tensor, value: Tensor) -> Tensor:
 	temp = query.bmm(key.transpose(1, 2))
 	scale = query.size(-1) ** 0.5
 	softmax = f.softmax(temp / scale, dim=-1)
@@ -90,7 +90,7 @@ class TransformerEncoder(nn.Module):
 
 	def forward(self, src: Tensor) -> Tensor:
 		seq_len, dimension = src.size(1), src.size(2)
-		src += position_encoding(seq_len, dimension)
+		src += position_encoding(seq_len, dimension, device=src.get_device())
 		for layer in self.layers:
 			src = layer(src)
 

src/training/train.py
@@ -59,7 +59,7 @@ def initialize(model_to_load=''):
 
     model = model.to(device)
 
-    criterion = torch.nn.CrossEntropyLoss() if NUM_CLASSES == 2 else torch.nn.BCEWithLogitsLoss()
+    criterion = torch.nn.CrossEntropyLoss()# if NUM_CLASSES == 2 else torch.nn.BCEWithLogitsLoss()
 
     optimizer = torch.optim.Adam(params=model.parameters(), 
                                  lr=LEARNING_RATE, 
@@ -119,8 +119,10 @@ def train_epoch(train_dataloader, model, criterion, optimizer, scheduler, perfor
 
         model.zero_grad()
 
+        print(inputs.size())
+
         logits = model(inputs, masks)
-        loss = criterion(logits, labels)
+        loss = criterion(torch.argmax(logits, dim=1).type(torch.FloatTensor), labels.type(torch.LongTensor))
         loss.backward()
         torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0) # Clip to prevent exploding gradients
         optimizer.step()